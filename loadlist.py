03c3c32ed2b0767ba0a913ac733146aa814df277
bornostojak <22800507+bornostojak@users.noreply.github.com>
2021-02-01 20:48:19 +0100

    Added frompath class method from instantiating PlayItem from file

--- loadlist.py
+++ loadlist.py
@@ -6,12 +6,19 @@ def load(filename=None):
 
     tree=None
     with open(filename, 'br') as file:
-        for enc in ['utf-8', 'windows-1250', 'windows-1252']:
-            try:
-                tree = ElementTree.fromstring(file.read().decode(enc))
-                tree.encoding = enc
-                break
-            except:
-                file.seek(0)
-                pass
+        tree = fireplay.PlayList.fromxml(file.read().decode('windows-1250'))
+#        for enc in ['utf-8', 'windows-1250', 'windows-1252']:
+#            try:
+#                #tree = ElementTree.fromstring(file.read().decode(enc))
+#                tree=fireplay.PlayList.fromxml(file.read().decode(enc))
+#                tree.encoding = enc
+#                break
+#            except:
+#                file.seek(0)
+#                pass
+
+
     return tree
+
+def loadtest():
+    return load(filename='testing/mixed.xml')
